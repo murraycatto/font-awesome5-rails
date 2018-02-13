06402939510d118401f7b911b30077ab95095011
Murray Catto <murraycatto@live.co.za>
2018-02-13 21:29:38 +0200

    update verision

--- Gemfile
+++ Gemfile
@@ -6,5 +6,4 @@ gemspec
 group :development do
   gem "guard"
   gem "guard-minitest"
-  gem "pry"
 end
--- lib/font-awesome5-rails/version.rb
+++ lib/font-awesome5-rails/version.rb
@@ -1,6 +1,6 @@
 module FontAwesome5
   module Rails
     FA_VERSION = "5.0.6"
-    VERSION = "4.7.0.3"
+    VERSION = "1.0.0"
   end
 end
